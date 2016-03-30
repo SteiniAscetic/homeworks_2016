2d87a4c17647ce8e6d6e4fbf17d5dc65ae750dbc
SteiniAscetic <steinreggae@yandex.ru>
2016-03-30 22:04:27 +0300

    In order from "Z" to "A"

---
+++ Homeworks/src/Lesson160329/Homework_1_2.java
@@ -0,0 +1,13 @@
+package Lesson160329;
+
+public class Homework_1_2 {
+
+	public static void main(String[] args) {
+		
+		for (char i = 'Z'; i >= 'A'; i--)
+			
+			System.out.println(i + "");
+
+	}
+
+}
