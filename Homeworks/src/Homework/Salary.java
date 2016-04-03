a715ca1d01eeb8a8c37bb70f213114e3bdc405e9
SteiniAscetic <steinreggae@yandex.ru>
2016-04-03 21:36:07 +0300

    Clean salary

---
+++ Homeworks/src/Homework/Salary.java
@@ -0,0 +1,15 @@
+package Homework;
+
+public class Salary {
+
+	public static void main(String[] args) {
+		
+		float a = 45.0f;
+		float b = a * 0.13f;
+		float c = a - b;
+		
+		System.out.println(c);
+
+	}
+
+}
