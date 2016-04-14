c8ae96cfbc9067f7bffe78b3b665a524d40fabc2
student <student@eprupetw6165.petersburg.epam.com>
2016-04-14 21:45:50 +0300

    classwork160414

---
+++ Homeworks/src/classwork160414/StringAreImmutable.java
@@ -0,0 +1,20 @@
+package classwork160414;
+
+public class StringAreImmutable {
+
+	public static void main(String[] args) {
+		
+		int i = 0;
+		
+		String s = "hello";
+		String w = "world";
+		
+		s = s + w;
+		
+		System.out.println(s);
+		
+		
+		
+	}
+
+}
